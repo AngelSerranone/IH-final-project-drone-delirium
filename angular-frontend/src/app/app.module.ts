08c2bce65d978bdec457972474c5224a5bc21cb4
Angel Serrano <angel.serranoguerales@gmail.com>
2021-03-05 21:51:07 +0100

    Added form, nav bar, and some images

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PhotoComponent } from './components/photo/photo.component';
+import { VideoComponent } from './components/video/video.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent,
+    VideoComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-frontend/src/app/components/contact/contact.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  form: FormGroup;
+
+  name: FormControl;
+  lastName: FormControl;
+  telephoneNumber: FormControl;
+  email: FormControl;
+  message: FormControl;
+
+  constructor() {
+    
+    // Initialize formControl
+    this.name = new FormControl ('', [Validators.required]);
+    this.lastName = new FormControl ('', [Validators.required]);
+    this.telephoneNumber = new FormControl ('', [Validators.required]);
+    this.email = new FormControl ('', [Validators.required]);
+    this.message = new FormControl ('', [Validators.required]);
+
+    // Initialize formGroup
+    this.form = new FormGroup({
+      name: this.name,
+      lastName: this.lastName,
+      telephoneNumber: this.telephoneNumber,
+      email: this.email,
+      message: this.message,
+    });
+   }
+
+  ngOnInit(): void {
+  }
+
+  submit(formDirective: FormGroupDirective): void {
+    
+  }
+
+}
