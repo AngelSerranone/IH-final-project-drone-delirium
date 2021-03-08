6f3b33016cb331006a5839121df639d2424bf1a1
Angel Serrano <angel.serranoguerales@gmail.com>
2021-03-08 01:30:41 +0100

    photo service works. contact service works. Videos inserted in website.

---
+++ contact-service/src/main/java/com/ironhack/contactservice/controller/dto/ContactDto.java
@@ -0,0 +1,2 @@
+package com.ironhack.contactservice.controller.dto;public class ContactDto {
+}
---
+++ contact-service/src/main/java/com/ironhack/contactservice/repository/ContactRepository.java
@@ -0,0 +1,2 @@
+package com.ironhack.contactservice.repository;public interface ContactRepository {
+}
